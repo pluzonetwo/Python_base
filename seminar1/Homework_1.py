0e7a0ebf2c0e9398eaeb4fdd760e581a217da322
Konstantin Os <permkonstantin@gmail.com>
2023-06-20 21:27:55 +0500

    Task_4

--- seminar1/Homework_1.py
+++ seminar1/Homework_1.py
@@ -56,3 +56,19 @@
 #         print('Yes!')
 #     else:
 #         print('No!')
+
+# Задача 8: Требуется определить, можно ли от шоколадки размером n × m долек отломить k долек, 
+# если разрешается сделать один разлом по прямой между дольками (то есть разломить шоколадку на два прямоугольника).
+# *Пример:*
+# 3 2 4 -> yes
+# 3 2 1 -> no
+
+# Решение:
+# lengthOfChocolate = int(input('Введите длину шоколадки: '))
+# widthOfChocolate = int(input('Введите ширину шоколадки: '))
+# numOfSlices = int(input('Сколько долек вы хотите отломить: '))
+# areaOfChocolate = lengthOfChocolate * widthOfChocolate
+# if areaOfChocolate % numOfSlices == 0:
+#     print('Yes!')
+# else:
+#     print('No!')
