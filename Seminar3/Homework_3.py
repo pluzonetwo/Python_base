28242f6312c3f31d4563b88bc5bb72f4a1246224
Konstantin Os <permkonstantin@gmail.com>
2023-06-24 16:27:58 +0500

    HW3_Task_16_ready

---
+++ Seminar3/Homework_3.py
@@ -0,0 +1,23 @@
+# Задача 16: Требуется вычислить, сколько раз встречается некоторое число X в массиве A[1..N]. 
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве. 
+# В последующих  строках записаны N целых чисел A. Последняя строка содержит число X
+# *Пример:*
+# 5
+#     1 2 3 4 5
+#     3
+#     -> 1
+# import random
+
+# list = []
+# quantity_of_list = int(input('Input quantity elements of list: '))
+# numX = int(input('Input number X: '))
+# count = 0
+
+# for i in range(quantity_of_list):
+#     list.append(random.randint(1, quantity_of_list))
+#     if numX == list[i]:
+#         count += 1
+
+# print(list)
+# print(numX)
+# print(count)
