68a0e384e241e56f79368cee637b3421b29890c8
Konstantin Os <permkonstantin@gmail.com>
2023-06-24 16:50:15 +0500

    HW3_Task_18_ready

--- Seminar3/Homework_3.py
+++ Seminar3/Homework_3.py
@@ -6,7 +6,7 @@
 #     1 2 3 4 5
 #     3
 #     -> 1
-# import random
+import random
 
 # list = []
 # quantity_of_list = int(input('Input quantity elements of list: '))
@@ -21,3 +21,26 @@
 # print(list)
 # print(numX)
 # print(count)
+
+# Задача 18: Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X. 
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве. 
+# В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
+# *Пример:*
+# 5
+#     1 2 3 4 5
+#     6
+#     -> 5
+
+# list = []
+# quantity_of_list = int(input('Input quantity elements of list: '))
+# numX = int(input('Input number X: '))
+# result = 1
+
+# for i in range(quantity_of_list):
+#     list.append(random.randint(1, quantity_of_list))
+#     if list[i] < numX and list[i] > result:
+#         result = list[i]
+
+# print(list)
+# print(numX)
+# print(result)
