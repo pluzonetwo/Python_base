b5b1cbbc26bbd30fcb6e1cfbeb3b88ff5b584d9d
Konstantin Os <permkonstantin@gmail.com>
2023-07-02 14:58:03 +0500

    HW_5_Task_1

---
+++ Seminar5/Practice_5.py
@@ -0,0 +1,96 @@
+# Генерация списка
+# string = input('Input: ')
+# list = [int(i) for i in string.split()]
+
+# Создание функций
+# def my_foo():
+#     pass
+
+# Структура программы (функции)
+# Любые осмысленные блоки программы нужно выводить в функции.
+# def vvod():
+#     pass
+
+# def vivod():
+#     pass
+
+# def solve():
+#     pass
+
+# massiv = vvod()
+# answer = solve()
+# vivod()
+
+# def kvadrat(arg1):
+#     # result = arg1 * arg1
+#     # return result
+#     return arg1 * arg1
+
+# n = int(input('Number: '))
+# print(kvadrat(n))
+
+# Хакер Василий получил доступ к классному журналу и
+# хочет заменить все свои минимальные оценки на
+# максимальные. Напишите программу, которая
+# заменяет оценки Василия, но наоборот: все
+# максимальные – на минимальные.
+# Input: 5 -> 1 3 3 3 4
+# Output: 1 3 3 3 1
+
+# string = input('Input: ')
+# list = [int(i) for i in string.split()]
+
+# arr = [1, 3, 3, 3, 4]
+
+# def find(array):
+#     maximum = max(array)
+#     minimum = min(array)
+        
+#     for i in range(len(array)):
+#         if array[i] == maximum:
+#             array[i] = minimum
+    
+#     return array
+
+# print(arr)
+# print(find(arr))
+
+# Напишите функцию, которая принимает одно число и
+# проверяет, является ли оно простым
+# Напоминание: Простое число - это число, которое
+# имеет 2 делителя: 1 и n(само число)
+# Input: 5
+# Output: yes 
+
+# number = int(input('Input number: '))
+
+# def isSimple(num):
+#     for i in range(2, num):
+#         if num % i == 0:
+#             return False
+#     return True
+
+# print(isSimple(number))
+
+# def isSimple(num):
+#     for i in range(2, num // 2 + 1):
+#         if num % i == 0:
+#             return False
+#     return True
+
+# print(isSimple(number))
+
+# Дано натуральное число N и
+# последовательность из N элементов.
+# Требуется вывести эту последовательность в
+# обратном порядке.
+# Примечание. В программе запрещается
+# объявлять массивы и использовать циклы
+# (даже для ввода и вывода).
+# Input: 2 -> 3 4
+# Output: 4 3
+
+# работаем со строкой
+
+
+
