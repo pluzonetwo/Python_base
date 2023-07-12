270a210f89ca08b37fce734039558f012229dd6c
Konstantin Os <permkonstantin@gmail.com>
2023-07-12 22:20:36 +0500

    HW_7_ready

--- Seminar7/Homework_7.py
+++ Seminar7/Homework_7.py
@@ -31,12 +31,36 @@
 #             return False
 #     return True
 
-# output = is_rythm(generated_string(phrase), is_count_lowels(generated_string(phrase), vowels), vowels)
-
 # def res(data):
 #     if data:
 #         print('Парам пам-пам')
 #     else:
 #         print('Пам парам')
 
+# output = is_rythm(generated_string(phrase), is_count_lowels(generated_string(phrase), vowels), vowels)
+
 # res(output)
+
+# Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), которая принимает в качестве аргумента функцию, 
+# вычисляющую элемент по номеру строки и столбца. Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, которые должны 
+# быть распечатаны. Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля). Примечание: бинарной операцией называется любая 
+# операция, у которой ровно два аргумента, как, например, у операции умножения.
+# *Пример:*
+# **Ввод:** `print_operation_table(lambda x, y: x * y) ` 
+# **Вывод:**
+# 1 2 3 4 5 6
+# 2 4 6 8 10 12
+# 3 6 9 12 15 18
+# 4 8 12 16 20 24
+# 5 10 15 20 25 30
+# 6 12 18 24 30 36
+
+# def print_operation_table(func, num_rows = 6, num_cols = 6):
+#   for i in range(1, num_rows):
+#     string = ' '
+#     for j in range(1, num_cols):
+#       string = string + str(func(i, j)) + ' '
+#     print(string + '\t')
+
+# print_operation_table(lambda x, y: x * y)
+
